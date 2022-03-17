d75d3e86f9330c1b25fecdb3efa1e68fc6db5980
yoojin1 <101783108+yoojin1@users.noreply.github.com>
2022-03-17 12:56:26 +0900

    Add files via upload

---
+++ 20220317.py
@@ -0,0 +1,52 @@
+#1
+print('Hello World')
+
+#2
+print("Mary's cosmetics")
+
+#3
+print('신씨가 소리질렀다. "도둑이야".')
+
+#4
+print('"C:\\Windows"')
+
+#5
+print("안녕하세요.\n만나서\t\t반갑습니다.")
+# \n은 줄 띄우기, \t는 tab만큼 공백을 만들어주는 역할 
+
+#51
+movie_rank = ['닥터 스트레인지', '스플릿', '럭키']
+print("{} {} {}".format(movie_rank[0],movie_rank[1],movie_rank[2]))
+
+#52
+movie_rank.append("배트맨")
+print("{} {} {} {}".format(movie_rank[0],movie_rank[1],movie_rank[2],movie_rank[3]))
+
+#53
+movie_rank.insert(1, "슈퍼맨")
+print("{} {} {} {} {}".format(movie_rank[0],movie_rank[1],movie_rank[2],movie_rank[3], movie_rank[3]))
+
+#58
+nums = [1, 2, 3, 4, 5]
+a = nums[0]+nums[1]+nums[2]+nums[3]+nums[4]
+print(a)
+
+#59
+cook= ["피자","김밥","만두","양념치킨","족발","피자","김치만두","쫄면","소시지","라면","팥빙수","김치"]
+print(len(cook))
+
+#60
+a /= 5.0
+print(a)
+
+#102
+#False
+
+#103
+#True
+
+#104
+#True
+
+#105
+#True
